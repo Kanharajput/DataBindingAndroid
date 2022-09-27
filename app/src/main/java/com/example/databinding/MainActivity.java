53377b4d049b0573859578adf74ac571e357067e
kanharajput91 <kanharajput91@gmail.com>
2022-09-27 18:45:42 +0530

    example of databinding

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.databinding.databinding.ActivityMainBinding;
 
@@ -11,14 +12,33 @@ public class MainActivity extends AppCompatActivity {
 
     // activity binding object
     ActivityMainBinding binding;
+    String[] first_names;
+    String[] second_names;
+    int total_names;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // set the layout using and get the reference in binding object
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        binding.firstNameTxt.setText("kanha");
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // set default names
+        binding.firstNameTxt.setText("Kanha");
         binding.secondNameTxt.setText("Tomar");
 
+        // getting the array of names from resources
+        first_names = getResources().getStringArray(R.array.first_names);
+        second_names = getResources().getStringArray(R.array.second_names);
+        total_names = first_names.length;
+
+        binding.nextNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.firstNameTxt.setText(first_names[count%total_names]);
+                binding.secondNameTxt.setText(second_names[count%total_names]);
+                count++;
+            }
+        });
     }
 }
