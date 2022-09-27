2b4f876740cdff79f28b632472d5458de669a3ec
kanharajput91 <kanharajput91@gmail.com>
2022-09-27 18:08:20 +0530

    Elements directly accessed via data binding

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import android.os.Bundle;
 
+import com.example.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // activity binding object
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // set the layout using and get the reference in binding object
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.firstNameTxt.setText("kanha");
+        binding.secondNameTxt.setText("Tomar");
+
     }
 }
