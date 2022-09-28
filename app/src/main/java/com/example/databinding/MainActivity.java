6a5eb79c97ec5c35fe52aeca6e9c332d0bb9f783
kanharajput91 <kanharajput91@gmail.com>
2022-09-28 18:17:52 +0530

    bind the data with layout, static binding

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     String[] first_names;
     String[] second_names;
     int total_names;
-    int count = 0;
+    int count = 1;           // because 0 values are set as default value
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         // set the layout using and get the reference in binding object
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        // set default names
-        binding.firstNameTxt.setText("Kanha");
-        binding.secondNameTxt.setText("Tomar");
+        // set default name
+        binding.setFirstName("Kanha");
+        binding.setSecondName("Tomar");
 
         // getting the array of names from resources
         first_names = getResources().getStringArray(R.array.first_names);
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
         binding.nextNameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                binding.firstNameTxt.setText(first_names[count%total_names]);
-                binding.secondNameTxt.setText(second_names[count%total_names]);
+                binding.setFirstName(first_names[count%total_names]);
+                binding.setSecondName(second_names[count%total_names]);
                 count++;
             }
         });
